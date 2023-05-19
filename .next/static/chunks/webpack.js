707d85cc7969c098a4e3b9e443a78abb7ce9e236
Dhruv Parmar <dhruv.parmar@yudiz.com>
2023-05-19 17:24:36 +0530

    issue: created Head component as SEO but was not working

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7739530e909f410c"; }
+/******/ 		__webpack_require__.h = function() { return "7a91f905b135f1ea"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,6 @@ import Hero from '@/components/Hero'
 import bookCover from '../../public/book_cover.jpg'
 import Link from 'next/link'
 import { saveAs } from 'file-saver'
-import SEO from '@/components/SEO'
 
 const ysabeau = Ysabeau({ subsets: ['latin'], weight: ['200', '300', '400', '500'], style: ['normal', 'italic'] })
 
@@ -15,7 +14,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <SEO title={'Bhagavad Gita'} />
+        <title>Bhagavad Gita</title>
+        <meta charset="utf-8"></meta>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name='keywords' content='Bhagavad Gita, BhagavadGita, bhagavad gita, BhagavadGita by Dhruv Parmar, Bhagavad Gita by Dhruv Parmar' />
+        <meta name="description" content="The Srimad Bhagavad Gita is the most sacred and popular book of Hindus. According to the scriptures, the Gita is the supreme book in all the books. The bhagavad gita book contains 700-verses which comes in the Mahabharata (the Indian epic). Gita means the song in Sanskrit and Srimad Bhagavad means the supreme lord. So the Srimad Bhagavad Gita means  ‘the song of the god’." />
+        <link rel="icon" href="/favicon.png" />
       </Head>
       <Hero />
 
