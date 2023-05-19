c544af5b2d159108e67dc44cb0062db0dfee79bd
Dhruv Parmar <dhruv.parmar@yudiz.com>
2023-05-19 17:22:12 +0530

    created Head component as SEO

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8e9e95c3e2aa5c33"; }
+/******/ 		__webpack_require__.h = function() { return "7739530e909f410c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/about/wallpapers.jsx
+++ src/pages/about/wallpapers.jsx
@@ -4,6 +4,7 @@ import { Ysabeau } from 'next/font/google'
 import Link from 'next/link'
 import Head from 'next/head'
 import WallpaperCard from '@/components/WallpaperCard'
+import SEO from '@/components/SEO'
 
 const ysabeau = Ysabeau({ subsets: ['latin'], weight: ['200', '300', '400', '500'], style: ['normal', 'italic'] })
 
@@ -11,12 +12,7 @@ function Wallpapers() {
   return (
     <>
         <Head>
-            <title>Wallpapers | Bhagavad Gita</title>
-            <meta charset="utf-8"></meta>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta name='keywords' content='Bhagavad Gita, BhagavadGita, bhagavad gita, BhagavadGita by Dhruv Parmar, Bhagavad Gita by Dhruv Parmar' />
-            <meta name="description" content="The Srimad Bhagavad Gita is the most sacred and popular book of Hindus. According to the scriptures, the Gita is the supreme book in all the books. The bhagavad gita book contains 700-verses which comes in the Mahabharata (the Indian epic). Gita means the song in Sanskrit and Srimad Bhagavad means the supreme lord. So the Srimad Bhagavad Gita means  ‘the song of the god’." />
-            <link rel="icon" href="/favicon.png" />
+            <SEO title={'Wallpapers | Bhagavad Gita'} />
         </Head>
         <section id="breadcrumbs" className={`breadcrumbs ${ysabeau.className}`}>
             <div className="container">
