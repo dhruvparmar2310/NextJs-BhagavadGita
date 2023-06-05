6c11017232ff0869afe531a5b7ecedd3b63d6dd7
Dhruv <134577662+yudiz-dhruv@users.noreply.github.com>
2023-06-05 19:07:33 +0530

    added getStaticProps method in [adhyayID].js file

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,15 +1,1 @@
-self.__NEXT_FONT_MANIFEST={
-  "pages": {
-    "/_app": [
-      "static/media/776235dcfdac5fac.p.woff2",
-      "static/media/9535a88fe8861650.p.woff2"
-    ],
-    "/about/[adhyay]/[adhyayID]": [
-      "static/media/776235dcfdac5fac.p.woff2",
-      "static/media/9535a88fe8861650.p.woff2"
-    ]
-  },
-  "app": {},
-  "appUsingSizeAdjust": false,
-  "pagesUsingSizeAdjust": false
-}
+self.__NEXT_FONT_MANIFEST={pages:{"/about":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/about/iskcon":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/about/lord-krishna":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/about/wallpapers":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/bhajan":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/contact":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/_app":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/about/[adhyay]":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"],"/about/[adhyay]/[adhyayID]":["static/media/776235dcfdac5fac.p.woff2","static/media/9535a88fe8861650.p.woff2"]},app:{},appUsingSizeAdjust:!1,pagesUsingSizeAdjust:!1};
